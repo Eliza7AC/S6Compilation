6e12c9a5751949dc832227167402d43d62170f5a
slash <slash@slash.com>
2022-01-23 17:17:59 +0100

    foo

--- src/Analyseur.java
+++ src/Analyseur.java
@@ -1,66 +1,124 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Analyseur {
 
-    List<String> words = new ArrayList<String>();
+    final private int LONG_MAX_IDENT = 20;
+    final private int LONG_MAX_CHAINE = 50;
+    final private int NB_MOTS_RESERVES = 7;
 
+    public File SOURCE;
+    public char CARLU; // dernier char lu
+    public boolean PREVIOUS_CHAR_IS_SIMPLE = false;
+    public boolean IS_COMPOSED_SYMBOL = false;
+    public int CARLU_INDEX;
+    public Integer NOMBRE; // dernier nbre lu
+    public String CHAINE; // dernier identificateur, mot-clé, ... lu
+    public int NUM_LIGNE; // num ligne lue pour les ERREUR
+
+    public ArrayList<String> TABLE_MOTS_RESERVES = new ArrayList<String>(
+            Arrays.asList("DEBUT", "ECRIRE", "FIN", "LIRE", "SI", "VAR")
+    );
+
+    public ArrayList<String> ERREURS = new ArrayList<String>();
+
+
+    /**
+     * constructeur
+     */
     Analyseur(File file) throws FileNotFoundException {
+        SOURCE = file;
+        LIRE_CAR();
+    }
 
-        Scanner sc = new Scanner(file);
 
-        /**
-         * scan du programme source mot par mot
-         */
-        while (sc.hasNextLine()){
-            String line = sc.nextLine();
+    public void LIRE_CAR() throws FileNotFoundException {
+        Scanner sc = new Scanner(SOURCE);
+        NUM_LIGNE = 0;
+        ERREURS.add("null"); // nb error = index 0
+        ERREURS.add("fin de fichier atteinte"); // nb error = index 1
 
-//            String[] wordsArray = line.split("\s");
-//            for (int i = 0; i < wordsArray.length; i++) {
-//                if (!wordsArray[i].equals("")){ // n'ajoute pas les espaces
-//                    words.add(wordsArray[i]);
-//                }
-//            }
+        try{
+            /**
+             * scan char by char
+             */
+            while (sc.hasNextLine()){
+                NUM_LIGNE++;
+                CARLU_INDEX = 0;
 
-            for (Character c : line.toCharArray()){
-                System.out.println("char " + c + " isMereSymbol = " + isMereSymbol(c));
+                String strLine = sc.nextLine();
+                char[] charLine = strLine.toCharArray();
+
+                for (int i = 0; i < charLine.length; i++) {
+                    CARLU_INDEX++;
+                    CARLU = charLine[i];
+                    System.out.println(CARLU + " AT INDEX " + CARLU_INDEX);
+                }
             }
+
+        } catch (Exception e){
+            ERREUR(0); // en cas d'erreur en scannant
+        }
+        System.out.println(NUM_LIGNE);
+        ERREUR(1); // lorsque la fin du fichier est lue
+
+    }
+
+
+    public void ERREUR(int nbError){
+        System.out.println("erreur n°" + nbError +
+                " -- ligne " + NUM_LIGNE + " char " + CARLU_INDEX +
+                " -- message: " + ERREURS.get(nbError));
+    }
+
+
+
+    /**
+     * unités lexicales
+     */
+    public boolean isInteger(Character c){
+        char[] arrayOfNb = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        for (Character nb : arrayOfNb){
+            if (nb == c){
+                PREVIOUS_CHAR_IS_SIMPLE = false;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isMereSymbol(Character c){
+        if(c == ',' || c == ';' || c == '.' || c== ':' ||
+                c == '(' || c == ')' || c == '<' || c == '>' ||
+                c == '+' || c == '-' || c == '*' || c == '/'){
+
+            // if previous char is simple && actual char is simple
+            if (PREVIOUS_CHAR_IS_SIMPLE = true){
+                IS_COMPOSED_SYMBOL = true;
+            }
+            PREVIOUS_CHAR_IS_SIMPLE = true;
+            return true;
+        }
+        else {
+            return false;
         }
 
     }
 
-    public boolean isMereSymbol(Character c){
-        return c == ',' || c == ';' || c == '.' || c== ':' ||
-                c == '(' || c == ')' || c == '<' || c == '>' ||
-                c == '+' || c == '-' || c == '*' || c == '/';
-    }
-
-    public boolean isComposedSymbol(String s){
-        return s == "<=" || s == ">=" || s == "<>" || s == ":=";
-    }
-
     public boolean isSpace(Character c){
-        return c==' ';
+        if (c== ' '){
+            PREVIOUS_CHAR_IS_SIMPLE = false;
+            return true;
+        }
+        return false;
     }
 
 
-
-    /** todo 3 cas ouvrants/fermants: () <> {}
-     * todo paramètres => adresse des variables enregistrés
-     * todo reconnaître les nombres
-     */
-
-
-
-    public List<String> getWords() {
-        return words;
-    }
+    // todo sauter séparateurs, reco_entier, ...
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,60 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        // chargement du programme simulé par un fichier texte
+        // chargement du programme (fichier texte)
         Analyseur analyseur = new Analyseur(new File("src/JeuxEssais/test"));
 
-        System.out.println(" ############################ ");
-        System.out.println("mots du programme: ");
-        System.out.println(analyseur.getWords().toString());
-        System.out.println(" ############################ ");
-
-        Identificateur identificateurs = new Identificateur();
 
 
-        Compilateur compilateur = new Compilateur(analyseur,identificateurs);
-        compilateur.interpretation();
+//        System.out.println(" ############################ ");
+//        System.out.println("mots du programme: ");
+//        System.out.println(analyseur.getWords().toString());
+//        System.out.println(" ############################ ");
+//
+//        Identificateur identificateurs = new Identificateur();
+//
+//
+//        Compilateur compilateur = new Compilateur(analyseur,identificateurs);
+//        compilateur.interpretation();
+//
+//
+//        System.out.println(" ");
+//        Analyseur an = new Analyseur(new File("src/JeuxEssais/simplesSymboles"));
 
 
-        System.out.println(" ");
-        Analyseur an = new Analyseur(new File("src/JeuxEssais/simplesSymboles"));
 
 
+
+
+//        Scanner sc = new Scanner(new File("src/JeuxEssais/test"));
+//
+//        while (sc.hasNextLine()){
+//            String strLine = sc.nextLine();
+//            char[] charLine = strLine.toCharArray();
+//
+//            for (int i = 0; i < charLine.length; i++) {
+//                System.out.println(charLine[i]);
+//            }
+//        }
+
+//        System.out.println();
+//        char a = '9';
+//        System.out.println(Character.getNumericValue(a));
+//
+//        char[] arrayOfNb = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+//
+//        for (Character nb : arrayOfNb){
+//            if (nb == a){
+//                System.out.println("TRUE");
+//            }
+//
+//        }
+//        System.out.println("FIN");
 
 
     }
